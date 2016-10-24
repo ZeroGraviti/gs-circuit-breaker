0d669fccd9a1dd4eb44748019e9f54d68dcf2408
ZeroGraviti <sushant_sarkar@yahoo.com>
2016-10-24 09:09:09 -0500

    Fixed hystrix actuator dep - still showing Closed when fallback kicks in

--- complete/reading/src/main/java/hello/BookService.java
+++ complete/reading/src/main/java/hello/BookService.java
@@ -1,25 +1,25 @@
 package hello;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import java.net.URI;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class BookService {
 
-  private final RestTemplate restTemplate;
-
-  public BookService(RestTemplate rest) {
-    this.restTemplate = rest;
-  }
-
-  @HystrixCommand(fallbackMethod = "reliable")
+  @HystrixCommand(fallbackMethod = "reliable",
+	commandProperties={
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+        @HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE")
+        }
+	)
   public String readingList() {
+    RestTemplate restTemplate = new RestTemplate();
     URI uri = URI.create("http://localhost:8090/recommended");
 
-    return this.restTemplate.getForObject(uri, String.class);
+    return restTemplate.getForObject(uri, String.class);
   }
 
   public String reliable() {
